cd6cdcfbea01a3d1481e79ed3f9be5bcc646ee73
Phillip Hunter <phunter95@gmail.com>
2017-05-08 20:59:22 -0400

    API is working.

---
+++ Box-Writer-API/Controllers/BoxWriterController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Box_Writer_API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/BoxWriter")]
+    public class BoxWriterController : Controller
+    {
+        // GET api/boxwriter/
+        [HttpGet]
+        public string Get(String word, String label, Int32 spacing = 2, Int32 offset = 10)
+        {
+            if(word == null)
+            {
+                return "INVALID BOX: No Word Specified!";
+            }
+
+            if(label == null)
+            {
+                label = String.Empty;
+            }
+
+            BoxWriter _BoxWriter = new BoxWriter(word, label);
+            _BoxWriter.SetSpacing(spacing);
+            _BoxWriter.SetOffset(offset);
+
+            _BoxWriter.Boxify();
+
+            return _BoxWriter.ToString();
+        }
+    }
+}
